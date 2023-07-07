48878a355ff4c529a72ff7cdad593eeed2e1c0c7
Anshuman Thakur <anshumanth1997@gmail.com>
2023-07-07 15:30:09 +0530

    Update test_script.sh

--- test_script.sh
+++ test_script.sh
@@ -3,7 +3,7 @@
 set -x
 
 cd /home/ubuntu
-sudo git clone https://oauth2:glpat-EaboeKqZThcDvdv7YK_h@gitlab.com/testgroup7502524/oracle-oci.git
+sudo git clone https://<token>@gitlab.com/testgroup7502524/oracle-oci.git
 cd oracle-oci
 sudo chmod +x deploy.sh
 . deploy.sh
